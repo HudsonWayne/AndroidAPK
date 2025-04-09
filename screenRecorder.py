f1920510353541529181a43c3e7b765fa9fb86ff
hudsonnbenhuraa@gmail.com <HudsonWayne>
2025-04-09 14:09:23 +0200

    initial commit

--- screenRecorder.py
+++ screenRecorder.py
@@ -1,3 +1,10 @@
 import cv2
 import pyautogui
 from win32api import GetSystemMetrics
+import numpy as np
+import time
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+
+dim = (width, height)
